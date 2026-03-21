6ad838aab91aa225da3dd5e52115962a013819a3
ayrton47 <ayrtonfroidbise@yahoo.com>
2026-03-21 16:52:35 +0100

    Update Lead type to use leadActions instead of actions

--- lib/types.ts
+++ lib/types.ts
@@ -29,7 +29,7 @@ export interface Lead {
   source: LeadSource
   externalId?: string
   agentId: string
-  actions: LeadAction[]
+  leadActions: LeadAction[]
   createdAt: string
   updatedAt: string
 }
